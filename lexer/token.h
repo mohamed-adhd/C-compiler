9b6e59912703c4d2f017bbf22223d20e40c66329
mohamed amdouni <midouamdouni4@gmail.com>
2026-07-27 20:08:43 +0000

    more tokens

--- lexer/token.h
+++ lexer/token.h
@@ -2,5 +2,18 @@ typedef enum{
     TOKEN_INT,
     TOKEN_RETURN,
     TOKEN_IDENTIFIER,
-    TOKEN_NUMBER
+    TOKEN_NUMBER,
+    TOKEN_LPAREN,   
+    TOKEN_RPAREN,   
+    TOKEN_LBRACE,   
+    TOKEN_RBRACE,   
+    TOKEN_SEMICOLON,
+    TOKEN_EOF
 }tokentype;
+
+
+typedef struct{
+    tokentype type;
+    char *txt;
+    int val;
+}token;
