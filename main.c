56f17c90171f3825455a3dc767bb793bf997c5f2
mohamed amdouni <midouamdouni4@gmail.com>
2026-07-31 22:03:10 +0000

    full main cycle done

--- main.c
+++ main.c
@@ -1,14 +1,9 @@
 #include <c++/16.1.1/cstdio>
 #include <stdio.h>
 #include <stdlib.h>
+#include "codegen.h"
+#include "parse.h"
 #include "lexer/lexer.h"
-// 1. read source file (e.g. from argv[1]) into a string
-// 2. lexer_init + loop calling next_token until TOKEN_EOF, collect into a token array
-// 3. parser_init with that token array
-// 4. astnode *ast = parse_program(&p);
-// 5. FILE *out = fopen("output.s", "w");
-// 6. codegen_program(ast, out);
-// 7. fclose(out);
 int main(int argc, char *argv[]) {
 
     FILE *file = fopen("example.txt", "r");
@@ -25,10 +20,20 @@ int main(int argc, char *argv[]) {
     fclose(file);
     lexer lex_luther;
     lexer_init(&lex_luther,content);
+    token *tkr = malloc(size + 1);
     token temp =next_token(&lex_luther);
+    int i=1;
     while (temp.type!=TOKEN_EOF) {
-        
+        temp =next_token(&lex_luther);
+        tkr[i] = temp;
     }
+    paarser psr;
+    parser_init(&psr,tkr,size);
+    astnode *ast = parse_program(&psr);
+    FILE *out = fopen("output.s", "w");
+    codegen_program(ast, out);
+    fclose(out);
+
 
     return 0;
 }
