4360e22cb648c68f487f4633ec375f880404af35
mohamed amdouni <midouamdouni4@gmail.com>
2026-07-31 19:27:16 +0000

    codegen expression added

--- codegen/codegen.c
+++ codegen/codegen.c
@@ -4,4 +4,6 @@
 void codegen_program(astnode *program, FILE *out);
 void codegen_function(astnode *function, FILE *out);
 void codegen_statement(astnode *stmt, FILE *out);
-char *codegen_expression(astnode *expr, FILE *out);
+char *codegen_expression(astnode *expr, FILE *out) {
+    fprintf(out, "movl $%d, %%eax\n", expr->value);
+}
