066febc39fd64bf069a226f05ff1e9ce056a5e78
mohamed amdouni <midouamdouni4@gmail.com>
2026-08-01 17:29:41 +0000

    further changes to codegen expression

--- codegen/codegen.c
+++ codegen/codegen.c
@@ -14,9 +14,16 @@ void codegen_statement(astnode *stmt, FILE *out) {
     codegen_expression(stmt->expr,out);
     fprintf(out, "ret\n");
 }
-char *codegen_expression(astnode *stmt, FILE *out) {
-    codegen_expression(stmt->left, out);
-    fprintf(out, "push %%eax\n");
-    codegen_expression(stmt->right, out);
-    fprintf(out, "pop %%ecx\n");
+void codegen_expression(astnode *expr, FILE *out) {
+    if (expr->type == NODE_CONSTANT) {
+        fprintf(out, "movl $%d, %%eax\n", expr->value);
+
+    } else if (expr->type == NODE_BINARY_OP) {
+        codegen_expression(expr->left, out);
+        fprintf(out, "push %%eax\n");
+        codegen_expression(expr->right, out);
+        fprintf(out, "pop %%ecx\n");
+        if (expr->op == TOKEN_ADD) fprintf(out, "addl %%ecx, %%eax\n");
+        if (expr->op == TOKEN_SUB) fprintf(out, "subl %%ecx, %%eax\n");
+    }
 }
--- codegen/codegen.h
+++ codegen/codegen.h
@@ -5,4 +5,4 @@
 void codegen_program(astnode *program, FILE *out);
 void codegen_function(astnode *function, FILE *out);
 void codegen_statement(astnode *stmt, FILE *out);
-char *codegen_expression(astnode *expr, FILE *out); 
+void codegen_expression(astnode *expr, FILE *out);
