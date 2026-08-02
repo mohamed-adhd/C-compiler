14ae90273a3827d6bc73763c817eb392631fac5f
mohamed amdouni <midouamdouni4@gmail.com>
2026-08-02 19:25:09 +0000

    moved the itoa to program codegen to avoid multiple fprintf of it

--- codegen/codegen.c
+++ codegen/codegen.c
@@ -3,36 +3,6 @@
 
 
 void codegen_program(astnode *program, FILE *out) {
-    codegen_function(program->function, out);
-    //it will staty like this just for a while
-}
-void codegen_function(astnode *function, FILE *out) {
-    fprintf(out, ".globl %s\n%s:\n", function->name, function->name);
-    codegen_statement(function->body, out);
-}
-void codegen_statement(astnode *stmt, FILE *out) {
-    codegen_expression(stmt->expr,out);
-    fprintf(out, "ret\n");
-}
-//fuck that bitch , fuck this compiler, fuck c , fuck assembly , and fuck this god damn heat
-void codegen_expression(astnode *expr, FILE *out) {
-    if (expr->type == NODE_CONSTANT) {
-        fprintf(out, "movl $%d, %%eax\n", expr->value);
-    } else if (expr->type == NODE_BINARY_OP) {
-        codegen_expression(expr->left, out);
-        fprintf(out, "push %%eax\n");
-        codegen_expression(expr->right, out);
-        fprintf(out, "pop %%ecx\n");
-        if (expr->op == TOKEN_ADD) fprintf(out, "addl %%ecx, %%eax\n");
-        if (expr->op == TOKEN_SUB) fprintf(out, "subl %%ecx, %%eax\n");
-    }else if (expr->type == NODE_PRINT) {
-        codegen_expression(expr->expr, out);
-        codegen_print(out);
-
-    }
-
-}
-void codegen_print(FILE *out) {
     const char *ITOA_HELPER =
     "section .bss\n"
     "    count: resb 1\n"
@@ -67,4 +37,41 @@ void codegen_print(FILE *out) {
     "\n"
     "    leave\n"
     "    ret\n";
+    fprintf(out,"%s", ITOA_HELPER);
+    codegen_function(program->function, out);
+    //it will staty like this just for a while
+}
+void codegen_function(astnode *function, FILE *out) {
+    fprintf(out, ".globl %s\n%s:\n", function->name, function->name);
+    codegen_statement(function->body, out);
+}
+void codegen_statement(astnode *stmt, FILE *out) {
+    codegen_expression(stmt->expr,out);
+    fprintf(out, "ret\n");
+}
+//fuck that bitch , fuck this compiler, fuck c , fuck assembly , and fuck this god damn heat
+void codegen_expression(astnode *expr, FILE *out) {
+    if (expr->type == NODE_CONSTANT) {
+        fprintf(out, "movl $%d, %%eax\n", expr->value);
+    } else if (expr->type == NODE_BINARY_OP) {
+        codegen_expression(expr->left, out);
+        fprintf(out, "push %%eax\n");
+        codegen_expression(expr->right, out);
+        fprintf(out, "pop %%ecx\n");
+        if (expr->op == TOKEN_ADD) fprintf(out, "addl %%ecx, %%eax\n");
+        if (expr->op == TOKEN_SUB) fprintf(out, "subl %%ecx, %%eax\n");
+    }else if (expr->type == NODE_PRINT) {
+        codegen_expression(expr->expr, out);
+        codegen_print(out,expr->value);
+
+    }
+
+}
+void codegen_print(FILE *out,int val) {
+
+    fprintf(out, "mov rax, %d\n",val) ;
+    fprintf(out, "call itoa\n") ;
+
+
+
 }
--- codegen/codegen.h
+++ codegen/codegen.h
@@ -6,4 +6,4 @@ void codegen_program(astnode *program, FILE *out);
 void codegen_function(astnode *function, FILE *out);
 void codegen_statement(astnode *stmt, FILE *out);
 void codegen_expression(astnode *expr, FILE *out);
-void codegen_print(FILE *out);
+void codegen_print(FILE *out,int val);
