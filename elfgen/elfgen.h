faa079b5c495885fa60115b229f0b171e37fc563
mohamed amdouni <midouamdouni4@gmail.com>
2026-08-03 20:41:08 +0000

    elfgen:the 16 magic bytes mapped

--- elfgen/elfgen.h
+++ elfgen/elfgen.h
@@ -4,6 +4,7 @@
 
 #ifndef C_COMPILER_ELFGEN_H
 #define C_COMPILER_ELFGEN_H
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -15,6 +16,25 @@ void generate() {
     uint8_t *buff = (uint8_t *) malloc(file_size);
     size_t bytes_read = fread(buff, 1, file_size, file);
     fclose(file);
+    uint8_t *offset = (uint8_t *) malloc(120*sizeof(uint8_t));
+    offset[0]=0x7F;
+    offset[1]='E';
+    offset[2]='L';
+    offset[3]='F';
+    offset[4]=2;
+    offset[5]=1;
+    offset[6]=1;
+    offset[7]=0;
+    offset[8]=0;
+    offset[9]=0;
+    offset[10]=0;
+    offset[11]=0;
+    offset[12]=0;
+    offset[13]=0;
+    offset[14]=0;
+    offset[15]=0;
+    offset[16]=0;
+
 }
 
 #endif //C_COMPILER_ELFGEN_H
