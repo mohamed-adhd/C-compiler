c2c19bd6dd473265519e5d04518d7f05dcec5e61
mohamed amdouni <midouamdouni4@gmail.com>
2026-08-04 10:31:29 +0000

    added the token structs and types

--- masm/masm.h
+++ masm/masm.h
@@ -7,11 +7,25 @@
 typedef enum {
     OPERAND_NONE,OPERAND_REGISTER,OPERAND_IMMEDIATE,OPERAND_MEMORY,OPERAND_LABEL
 } operand_type;
-
 typedef enum {
     REG_RAX, REG_RCX, REG_RDX, REG_RBX,REG_RSP, REG_RBP, REG_RSI, REG_RDI
 } register_id;
-
+typedef enum{
+    TOKEN_NUMBER,
+    TOKEN_IDENTIFIER,
+    TOKEN_COMMA,
+    TOKEN_LBRACE,
+    TOKEN_RBRACE,
+    TOKEN_ver,
+    TOKEN_NEWLINE,
+    TOKEN_COLON,
+    TOKEN_EOF
+}tokentype;
+typedef struct{
+    tokentype type;
+    char *txt;
+    int val;
+}token;
 typedef struct {
     operand_type type;
     register_id reg;
@@ -19,8 +33,13 @@ typedef struct {
     char label[32];
 } operand;
 typedef struct {
+    int is_label;
+    char label_name[32];
+    instruction instr;
+} asm_line;
+typedef struct {
     char mnemonic[16];
     operand op1;
-    operand op2;     
+    operand op2;
 } instruction;
 #endif //C_COMPILER_MASM_H
