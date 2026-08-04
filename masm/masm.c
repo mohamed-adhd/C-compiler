e470efe8de199500849c26cd8aa13891aea684ad
mohamed amdouni <midouamdouni4@gmail.com>
2026-08-04 10:23:45 +0000

    incredible news! i have to make the lexer parser again for the assembly :D , such a fun thing to do in 40 degree heat

--- masm/masm.c
+++ masm/masm.c
@@ -1,3 +1,6 @@
 //
 // Created by bro on 8/4/26.
 //
+#include <stdio.h>
+#include <stdlib.h>
+#include "masm.h"
--- masm/masm.h
+++ masm/masm.h
@@ -4,5 +4,23 @@
 
 #ifndef C_COMPILER_MASM_H
 #define C_COMPILER_MASM_H
+typedef enum {
+    OPERAND_NONE,OPERAND_REGISTER,OPERAND_IMMEDIATE,OPERAND_MEMORY,OPERAND_LABEL
+} operand_type;
 
+typedef enum {
+    REG_RAX, REG_RCX, REG_RDX, REG_RBX,REG_RSP, REG_RBP, REG_RSI, REG_RDI
+} register_id;
+
+typedef struct {
+    operand_type type;
+    register_id reg;
+    long im;
+    char label[32];
+} operand;
+typedef struct {
+    char mnemonic[16];
+    operand op1;
+    operand op2;     
+} instruction;
 #endif //C_COMPILER_MASM_H
