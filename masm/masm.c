f2feba99f9a1a7a32b17ccedbd23821160231e09
mohamed amdouni <midouamdouni4@gmail.com>
2026-08-04 12:32:05 +0000

    line parser added , adding helpers

--- masm/masm.c
+++ masm/masm.c
@@ -106,5 +106,72 @@ asm_token *tokenizer(asm_lexer *l, int *count) {
     *count = i;
     return tokens;
 }
+void asm_parser_init(asm_parser *p, asm_token *tokens, int count) {
+    p->tokens = tokens;
+    p->pos = 0;
+    p->count = count;
+}
+operand asm_parse_operand(asm_parser *p) {
+    operand op = {0};
+    asm_token t = p->tokens[p->pos];
+    if (t.type == ASM_TOKEN_NUMBER) {
+        op.type = OPERAND_IMMEDIATE;
+        op.im = t.val;
+        p->pos++;
+        return op;
+    }
 
+    if (t.type == ASM_TOKEN_LBRACE) {
+        p->pos++;
+        asm_token inner = p->tokens[p->pos];
+        op.type = OPERAND_MEMORY;
+        strcpy(op.label, inner.txt);
+        p->pos++;
+        p->pos++; // started as a c compiler , now its , whatever the fuck this is
+        return op;
+    }
+
+    if (t.type == ASM_TOKEN_IDENTIFIER) {
+        if (is_register_name(t.txt)) {
+            op.type = OPERAND_REGISTER;
+            op.reg = register_from_name(t.txt);
+        } else {
+            op.type = OPERAND_LABEL;
+            strcpy(op.label, t.txt);
+        }
+        p->pos++;
+        return op;
+    }
+
+    fprintf(stderr, "ay you fucked up the parser\n");
+    exit(1);
+}
+asm_line asm_parse_line(asm_parser *p) {
+    asm_line line = {0};
+    asm_token first = p->tokens[p->pos];
+    if (first.type == ASM_TOKEN_IDENTIFIER &&
+        p->tokens[p->pos + 1].type == ASM_TOKEN_COLON) {
+        line.is_label = 1;
+        strcpy(line.label_name, first.txt);
+        p->pos++;
+        p->pos++;
+        return line;
+    }
+    line.is_label = 0;
+    strcpy(line.instr.mnemonic, first.txt);
+    p->pos++;
+    if (p->tokens[p->pos].type == ASM_TOKEN_NEWLINE ||p->tokens[p->pos].type == ASM_TOKEN_EOF) {
+        line.instr.op1.type = OPERAND_NONE;
+        line.instr.op2.type = OPERAND_NONE;
+        return line;
+    }
+    line.instr.op1 = asm_parse_operand(p);
+    if (p->tokens[p->pos].type == ASM_TOKEN_COMMA) {
+        p->pos++; 
+        line.instr.op2 = asm_parse_operand(p);
+    } else {
+        line.instr.op2.type = OPERAND_NONE;
+    }
+    return line;
+}
 
