8883fe60288432aeb578d56185ef9945f4ee226f
mohamed amdouni <midouamdouni4@gmail.com>
2026-08-08 12:05:43 +0000

    codegen updated

--- codegen/codegen.c
+++ codegen/codegen.c
@@ -1,15 +1,9 @@
 #include "codegen.h"
 #include <stdio.h>
-
+#include <string.h>
 #include "elfgen.h"
 void codegen_program(astnode *program, FILE *out) {
     const char *ITOA_HELPER =
-    "section .data\n"
-    "count: db 0\n"
-    "itoa_buffer: times 12 db 0\n"
-    "itoa_len: db 0\n"
-    "\n"
-    "section .text\n"
     "itoa:\n"
     "    push rbp\n"
     "    mov rbp, rsp\n"
@@ -37,10 +31,16 @@ void codegen_program(astnode *program, FILE *out) {
     "\n"
     "    leave\n"
     "    ret\n";
-fprintf(out, "BITS 64\norg 0x400078\n");
+    fprintf(out, "BITS 64\n");
+    fprintf(out, "org 0x400078\n");
+    fprintf(out, "section .data\n");
+    fprintf(out, "count: db 0\n");
+    fprintf(out, "itoa_buffer: times 12 db 0\n");
+    fprintf(out, "itoa_len: db 0\n");
+    if (program->function->body->type == NODE_PRINT &&program->function->body->expr->type == NODE_STRING) {fprintf(out, "str0 db \"%s\"\n",program->function->body->expr->string);}
+    fprintf(out, "\nsection .text\n");
     codegen_function(program->function, out);
     fprintf(out, "%s", ITOA_HELPER);
-
 }
 void codegen_function(astnode *function, FILE *out) {
     (void)function;
@@ -52,11 +52,12 @@ void codegen_function(astnode *function, FILE *out) {
 }
 void codegen_statement(astnode *stmt, FILE *out) {
     if (stmt->type == NODE_PRINT) {
+        codegen_print(out, stmt->expr);
+        return;}
+    if (stmt->type == NODE_RETURN) {
         codegen_expression(stmt->expr, out);
-        codegen_print(out,stmt->value);
         return;
     }
-    codegen_expression(stmt->expr,out);
 }
 //fuck that bitch , fuck this compiler, fuck c , fuck assembly , and fuck this god damn heat
 void codegen_expression(astnode *expr, FILE *out) {
@@ -68,19 +69,28 @@ void codegen_expression(astnode *expr, FILE *out) {
         codegen_expression(expr->right, out);
         fprintf(out, "pop rcx\n");
         if (expr->op == TOKEN_ADD) fprintf(out, "add rax, rcx\n");
-        if (expr->op == TOKEN_SUB) fprintf(out, "sub rcx, rax\nmov rax, rcx\n");
+        if (expr->op == TOKEN_SUB) fprintf(out, "sub rcx, rax\nmov rax, rcx\n");}}
+void codegen_print(FILE *out, astnode *expr) {
+
+    if (expr->type == NODE_CONSTANT ||
+        expr->type == NODE_BINARY_OP) {
+        codegen_expression(expr, out);
+        fprintf(out, "call itoa\n");
+        fprintf(out,
+            "mov rax, 1\n"
+            "mov rdi, 1\n"
+            "lea rsi, [itoa_buffer]\n"
+            "movzx rdx, byte [itoa_len]\n"
+            "syscall\n"
+        );return;}
+    if (expr->type == NODE_STRING) {
+        fprintf(out,
+            "mov rax, 1\n"
+            "mov rdi, 1\n"
+            "lea rsi, [str0]\n"
+            "mov rdx, %zu\n"
+            "syscall\n",
+            strlen(expr->string)
+        );
     }
-}
-void codegen_print(FILE *out,int val) {
-    fprintf(out, "call itoa\n") ;
-    const char *s="mov rax,1\n"
-                "mov rdi,1\n"
-                "lea rsi,[itoa_buffer]\n"
-                "movzx rdx,[itoa_len]\n"
-                "syscall\n";
-    fprintf(out,"%s", s);
-
-
-
-
 }
